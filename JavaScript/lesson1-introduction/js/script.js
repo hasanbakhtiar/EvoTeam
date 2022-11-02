c0e2d51617ce0b5230177c553a93b19a1ffdf550
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-11-02 20:15:02 +0400

    js enter

---
+++ JavaScript/lesson1-introduction/js/script.js
@@ -0,0 +1,16 @@
+// document.write('hello world...');
+// console.log("Hello console");
+
+// debugger;
+// console.log("Hello1");
+// console.log("Hello2");
+// console.lasdog("Hello3");
+// console.log("Hello4");
+// console.log("Hello5");
+// one line comment
+
+/**
+ * more
+ * line 
+ * comment
+ */
---
+++ JavaScript/lesson2-datatypes/js/script.js
@@ -0,0 +1,46 @@
+// Data Types
+
+//Basic DT 
+// string
+var info = 5;
+console.log(info);
+
+// var infoName= "JavaScript";
+// var infoText = "Hello ";
+// console.log(infoText + infoName);
+
+// number
+// var infoOne = 5;
+// var infoTwo = "10";
+// console.log(infoOne + infoTwo);
+
+// boolean
+// true 
+// false 
+// var product = false;
+// if (product) {
+//     console.log('product have in stock');
+// }else{
+//     console.log('stock is empty');
+
+// }
+
+// Composite DT
+// Array
+// var product = ['phone','laptop','xbox',5,10,true];
+// console.log(product);
+// Object
+// var product = {
+//     brand:"samsung",
+//     model:'s22',
+//     new:true
+// }
+// console.log(product.model);
+
+// Special DT
+// undefined 
+var data = undefined;
+console.log(data);
+// null
+var data = null;
+console.log(null);
