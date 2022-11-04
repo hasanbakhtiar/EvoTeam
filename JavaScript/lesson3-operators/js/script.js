07aba73879db31e661aa14f941fe77364b354f5b
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-11-04 20:32:30 +0400

    operators

---
+++ JavaScript/lesson3-operators/js/script.js
@@ -0,0 +1,128 @@
+// operators
+// assignment operators
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+// var infoData = 10;
+// var info = 5;
+// info**=infoData; //=>    info = info+infoData
+// console.log(info);
+
+// arithmetic operators
+// +
+// -
+// *
+// /
+// **
+// %
+// ++
+// --
+
+// var infoOne = 5;
+// var infoTwo = 10;
+// var result = infoOne + infoTwo;
+// result--;
+// console.log(result);
+
+//  comparison operators
+// ==
+// ===
+// <
+// =<
+// >
+// =>
+// !=
+// !==
+// var infoOne = "5";
+// var infoTwo = 10;
+
+// if (infoOne > infoTwo) {
+//     console.log('this is true');
+// }else{
+//     console.log('this is false');
+// }
+
+// Logical operators
+// && - AND
+// || - OR
+// ! - NOT
+// if (!false || false || false || false || false) {
+//     console.log('this is true');
+// }else{
+//     console.log('this is false');
+// }
+// const data = {
+//     dUser:'info',
+//     dPass:'info123'
+// }
+// const userInput = document.querySelectorAll("input")[0];
+// const passInput = document.querySelectorAll("input")[1];
+// const btn = document.querySelector("button");
+// const infoAlert = document.querySelector('p');
+// btn.onclick = (e) => {
+//   e.preventDefault();
+//   if (!userInput.value || !passInput.value) {
+//     infoAlert.innerHTML = "please, fill the input";
+//     infoAlert.style.color = 'orange';
+//   }else{
+//     if (userInput.value === data.dUser && passInput.value === data.dPass) {
+//         infoAlert.innerHTML = "Login Successfully";
+//         infoAlert.style.color = 'green';
+//     }else{
+//         infoAlert.innerHTML = 'username or password is wrong!'
+//         infoAlert.style.color = 'red';
+
+//     }
+//   }
+// };
+
+
+// string operators
+// +
+// +=
+// var a = "Hello ";
+// var b = "World"
+// a+=b;
+// console.log(a);
+
+
+
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+
+// \            (tərs slash)(Qaçındırma operatorudur)
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+
+
+// ?:           (sual üst-üstə qoşa nöqtə)((şərt) ? ifadə1 : ifadə2)(ternary);
+// var color ='green';
+// color === "red" ? console.log('red is beautiful color') : color === "green"? console.log('green is beautiful color') : console.log('red very cool...');;
+// 5>10 ? console.log(true): console.log(false);
+// (5 >= 10) ? document.write("Hi") : document.write("Bye");
+// şərt            ? true                 : false
+
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+// delete       (objectin dəyərini silmək üçündür)
+// new          (yeni dəyər yaratməq üçündür);
+// this         (mövcud dəyəri işarələmək üçündür)
+// in           (dəyişkənin(object) daxilində dəyərin olub olmamasını yoxlamaq üçündür)
+// instanceof   (dəyişkənin(objectin xüsusiyyətinin) içində xüsusiyyət olub olmamasını yoxlamaq üçündür)
+
+
+
+
+
+    
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
+
+
