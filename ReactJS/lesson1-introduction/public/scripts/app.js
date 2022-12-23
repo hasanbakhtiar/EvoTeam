d00b73447a571955eca906023a3aa81d8d070ad4
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-12-23 21:01:03 +0400

    react enter

---
+++ ReactJS/lesson1-introduction/public/scripts/app.js
@@ -0,0 +1,4 @@
+"use strict";
+
+var temp = /*#__PURE__*/React.createElement("h1", null, "Hello React");
+ReactDOM.render(temp, document.querySelector('#root'));
---
+++ ReactJS/lesson1-introduction/src/index.js
@@ -0,0 +1,5 @@
+var temp =(
+    <h1>Hello React</h1>
+)
+
+ReactDOM.render(temp,document.querySelector('#root'));
